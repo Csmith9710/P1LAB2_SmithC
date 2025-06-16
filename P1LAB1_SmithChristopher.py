babd2d1caacd79ca4b08a5c9339a17625034a52b
Csmith9710 <smithc9710@student.faytechcc.edu>
2025-06-16 10:03:19 -0400

    Add files via upload

---
+++ P1LAB1_SmithChristopher.py
@@ -0,0 +1,9 @@
+# Christopher Smith
+# 16JUNE2025
+# P1LAB1
+# input your first and last name into the lines and it will run with the inputs you have entered!
+
+first_name = input('Enter your first name: ')
+last_name = input('Enter your last name: ')
+
+print('Hello,', first_name, last_name,'!', 'Welcome to CTI-110')
